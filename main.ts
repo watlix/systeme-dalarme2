26446f9626755372cad608d4d0a1a80a261d60f3
watlix <93144173+watlix@users.noreply.github.com>
2021-11-18 11:32:14 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,23 @@
-let etat_actuel = 0
 let nombre = 0
+let etat_actuel = 0
 let etat_passe = 0
+input.onButtonPressed(Button.A, function () {
+    nombre += -1
+    basic.clearScreen()
+})
 basic.forever(function () {
     let etat_passe2 = 0
-    etat_actuel = pins.digitalReadPin(DigitalPin.P16)
+    etat_actuel = pins.digitalReadPin(DigitalPin.P2)
     if (etat_actuel != etat_passe2) {
         if (etat_actuel == 1) {
             nombre += 1
         }
     }
     etat_passe = etat_actuel
+    if (1 < nombre) {
+        nombre = 1
+    }
     if (nombre == 1) {
         basic.showString("alarme")
-        if (input.buttonIsPressed(Button.A)) {
-            nombre += -1
-        }
     }
 })
